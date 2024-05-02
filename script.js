4ce09d78c58a65d1d1e7eb67f80430a6d68e93d8
Antonino <drakon9927@libero.it>
2024-05-02 16:35:22 +0200

    m4

--- script.js
+++ script.js
@@ -24,30 +24,38 @@ while (bombe.length < 16) {
 
 console.log(bombe);
 
-// Funzione con aggiungi bomba
 function creaQuadrato(contenuto) {
     // Creo un quadrato
     let square = document.createElement("div");
     square.classList.add("square");
     square.innerText = contenuto;
-   
 
-    // Aggiungo un event listener 
+    // Imposto gameover su false all'inizio di ogni nuova partita
+    let gameover = false;
 
+    // Aggiungo un event listener
     square.addEventListener("click", function(){
         console.log("cliccato", this); // Il this rappresenta square, un modo per creare la variabile all'interno dell'event listener
-        
-        if (bombe.includes(contenuto)){
-            this.classList.add("bomba");
-        }
 
-        else{
+        // Verifica se il gioco è già finito
+        if (!gameover) {
+            if (bombe.includes(contenuto)){
+                this.classList.add("bomba");
+
+                alert("Game Over - Il tuo punteggio è " + punteggio);
+
+                // Imposta gameover su true per indicare che il gioco è finito
+                gameover = true;
+
+                this.removeEventListener("click", gestisciClic);
+             }
+
             this.classList.add("nobomba");
             punteggio++;
             console.log("Punteggio:", punteggio);
         }
-        
-    })
+    });
 
+    // Restituisci il quadrato creato
     return square;
 }
