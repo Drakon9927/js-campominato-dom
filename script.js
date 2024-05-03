4c13cd3e2af4bb5dfc14d4f9ff4dbd6c52138954
Antonino <drakon9927@libero.it>
2024-05-03 10:27:02 +0200

    m4

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let griglia = document.getElementById(`grid`);
 let punteggio = 0;
+let gameover = false; // Spostato al di fuori della funzione creaQuadrato
 
 for (let i = 0; i <= 100; i++) {
-    
+
     let elemento = creaQuadrato(i);
 
     griglia.append(elemento);
-
-
-    
 }
 
 let bombe = [];
@@ -16,10 +14,9 @@ let bombe = [];
 while (bombe.length < 16) {
     let numeroCasuale = Math.floor(Math.random() * 100) + 1;
 
-    if (!bombe.includes(numeroCasuale)){
+    if (!bombe.includes(numeroCasuale)) {
         bombe.push(numeroCasuale)
     }
-
 }
 
 console.log(bombe);
@@ -30,31 +27,32 @@ function creaQuadrato(contenuto) {
     square.classList.add("square");
     square.innerText = contenuto;
 
-    // Imposto gameover su false all'inizio di ogni nuova partita
-    let gameover = false;
-
     // Aggiungo un event listener
-    square.addEventListener("click", function(){
-        console.log("cliccato", this); // Il this rappresenta square, un modo per creare la variabile all'interno dell'event listener
+    square.addEventListener("click", gestisciClic);
+
+    function gestisciClic() {
+        console.log("cliccato", this);
 
         // Verifica se il gioco è già finito
         if (!gameover) {
-            if (bombe.includes(contenuto)){
+            if (bombe.includes(contenuto)) {
                 this.classList.add("bomba");
 
                 alert("Game Over - Il tuo punteggio è " + punteggio);
-
-                // Imposta gameover su true per indicare che il gioco è finito
                 gameover = true;
 
-                this.removeEventListener("click", gestisciClic);
-             }
+                // Indica che il gioco è finito
+                this.classList.add("gamestop");
 
-            this.classList.add("nobomba");
-            punteggio++;
-            console.log("Punteggio:", punteggio);
+                // Rimuovi l'event listener
+                this.removeEventListener("click", gestisciClic);
+            } else {
+                this.classList.add("nobomba");
+                punteggio++;
+                console.log("Punteggio:", punteggio);
+            }
         }
-    });
+    }
 
     // Restituisci il quadrato creato
     return square;
